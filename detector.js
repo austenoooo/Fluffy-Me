f2722d32bf844fc15c4858f353abf71bd696f114
Austenoooo <yixuan.li683@dukekunshan.edu.cn>
2023-02-28 00:30:57 -0500

    implement blaze pose functions

---
+++ detector.js
@@ -0,0 +1,78 @@
+// HTML elements
+const video = document.getElementById("webcam");
+
+// detector
+var detector = undefined;
+
+loadModel();
+
+function loadModel() {
+  const model = poseDetection.SupportedModels.BlazePose;
+  const detectorConfig = {
+    runtime: 'mediapipe',
+    solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/pose'
+  };
+  poseDetection.createDetector(model, detectorConfig).then((loadedDetector) => {
+    detector = loadedDetector;
+    loadCamera();
+  });
+}
+
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+async function loadCamera() {
+  if (getUserMediaSupported()){
+    enableCam();
+  }
+  else{
+    console.warn("webcam is not supported by your browser");
+  }
+}
+
+function enableCam() {
+  if (!detector){
+    return;
+  }
+
+  const constraints = {
+    video: true,
+  }
+
+  //activate the webcam strem
+  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+    video.srcObject = stream;
+    // video.addEventListener("loadeddata", renderPrediction);
+    renderPrediction();
+  });
+}
+
+async function renderPrediction() {
+  await renderResult();
+
+  requestAnimationFrame(renderPrediction);
+}
+
+async function renderResult() {
+  if (video.readyState < 2){
+    await new Promise((resolve) => {
+      video.onloadeddata = () => {
+        resolve(video);
+      }
+    });
+  }
+
+  if (detector) {
+
+    try{
+      const poses = await detector.estimatePoses(video, {enableSmoothing: true});
+      console.log(poses)
+    }
+    catch (error) {
+      detector.dispose();
+      detector = undefined;
+      alert(error);
+    }
+  }
+}
