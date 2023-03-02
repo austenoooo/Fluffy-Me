3fa51af0664da1e84fe906d9c17a153e97e16dfd
Austenoooo <yixuan.li683@dukekunshan.edu.cn>
2023-03-02 01:23:24 -0500

    make the shader work

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 
 let scene, camera, renderer;
-let meterial;
+let material;
 let controls;
 let landmarks;
 let objects = [];
@@ -11,6 +11,8 @@ let objects = [];
 let videoWidth = 640;
 let videoHeight = 480;
 let depthMax = 5;
+
+let uniforms;
 
 function init() {
   scene = new THREE.Scene();
@@ -28,13 +30,31 @@ function init() {
   camera.lookAt(videoWidth/2, videoHeight/2, 2000);
   // controls.update();
 
-  meterial = new THREE.MeshBasicMaterial( {color: 0xffffff, wireframe: true} );
+
+  uniforms = {
+
+    amplitude: { value: 50.0 },
+    opacity: { value: 0.3},
+    color: { value: new THREE.Color( 0xffffff ) }
+
+  };
+
+  material = new THREE.ShaderMaterial( {
+
+    uniforms: uniforms,
+    vertexShader: document.getElementById( 'vertexshader' ).textContent,
+    fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+    blending: THREE.AdditiveBlending,
+    depthTest: false,
+    transparent: true
+
+  } );
 
   // let ball = new THREE.Mesh(new THREE.SphereGeometry(100, 32, 16), meterial);
   // scene.add(ball);
   // ball.position.set(0, 0, 0);
 
-  environmentMap();
+  // environmentMap();
 
   loop();
 }
@@ -60,10 +80,44 @@ function addHead(){
   if (nose.score > 0.5 && leftShoulder.score > 0.5) {
     let nosePos = new THREE.Vector3(nose.x, (videoHeight - nose.y), depthMax - nose.z);
     let leftShoulderPos = new THREE.Vector3(leftShoulder.x, (videoHeight - leftShoulder.y), depthMax - leftShoulder.z);
-    let r = nosePos.distanceTo(leftShoulderPos) * 0.2;
-    let head = new THREE.Mesh(new THREE.SphereGeometry(r, 32, 16), meterial);
+    let r = nosePos.distanceTo(leftShoulderPos) * 0.4;
+
+    let baseGeometry = new THREE.SphereGeometry(r, 64, 32);
+
+    baseGeometry.center();
+
+    const count = baseGeometry.attributes.position.count;
+
+    const displacement = new THREE.Float32BufferAttribute( count * 3, 3 );
+    baseGeometry.setAttribute( 'displacement', displacement );
+
+    const customColor = new THREE.Float32BufferAttribute( count * 3, 3 );
+    baseGeometry.setAttribute( 'customColor', customColor );
+
+    const color = new THREE.Color( 0xffffff );
+
+    for ( let i = 0, l = customColor.count; i < l; i ++ ) {
+
+      color.setHSL( i / l, 0.5, 0.5 );
+      color.toArray( customColor.array, i * customColor.itemSize );
+
+    }
+
+    const array = baseGeometry.attributes.displacement.array;
+
+    for ( let i = 0, l = array.length; i < l; i += 3 ) {
+
+      const time = Date.now() / 1000;
+
+      array[ i ] += 3 * ( 0.5 - Math.random());
+      array[ i + 1 ] += 3 * ( 0.5 - Math.random());
+      array[ i + 2 ] += 3 * ( 0.5 - Math.random());
+
+    }
+
+    let head = new THREE.Line(baseGeometry, material);
     // console.log(r);
-    console.log(nosePos);
+    // console.log(nosePos);
     scene.add(head);
     head.position.set(nosePos.x, nosePos.y, nosePos.z);
 
@@ -74,7 +128,7 @@ function addHead(){
     // let nosePos = new THREE.Vector3(nose.x, nose.y, nose.z * videoWidth);
     // let rightShoulderPos = new THREE.Vector3(rightShoulder.x, rightShoulder.y, rightShoulder.z * videoWidth);
     // let r = nosePos.distanceTo(rightShoulderPos) * 0.8;
-    // let head = new THREE.Mesh(new THREE.SphereGeometry(r, 32, 16), meterial);
+    // let head = new THREE.Mesh(new THREE.SphereGeometry(r, 32, 16), material);
     // scene.add(head);
     // head.position.set(nosePos);
   }
